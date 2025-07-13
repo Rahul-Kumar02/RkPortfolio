c4615ba0eed67eb067cdd3165073def04048ce06
kumar rahul <rahulkacoding@gmail.com>
2025-07-13 09:23:04 +0530

    updated ui in certfication page

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -4,8 +4,8 @@ import css from "../../public/css-certificate.png";
 
 function Certifications() {
   return (
-  <div className="min-h-screen md:min-h-screen lg:min-h-screen flex items-center justify-center flex-col gap-20 px-4 py-20">
-    <h1 className="text-2xl w-full ">Certifications</h1>
+  <div className="min-h-screen md:min-h-screen lg:min-h-screen flex items-center justify-center flex-col gap-20 px-4 py-20 lg:px-20">
+    <h1 className="text-2xl w-full">Certifications</h1>
       <div className="w-[100%] h-[100%]  flex flex-col md:flex-row md:flex lg:flex lg:flex-row gap-5 items-center justify-center">
       <div className="w-[300px] h-[200px] shadow-2xl rounded-2xl">
         <img
